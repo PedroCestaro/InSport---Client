f8d007758fa007c3161a3e75aaad8a875982190f
pedro.cestaro <pedro.henrique@am4.com.br>
2024-02-28 20:53:42 -0300

    feature: Criando o header da landing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageHeaderComponent } from './landing-page-header/landing-page-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageHeaderComponent
   ],
   imports: [
     BrowserModule,
